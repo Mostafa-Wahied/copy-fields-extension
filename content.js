f29c48cc7abf87b41e942a0a756d343296afc20f
mostafa-wahied <mostafawahied@gmail.com>
2023-06-14 15:21:42 -0700

    added fields in popup for user to enter fields, added more fields

--- content.js
+++ content.js
@@ -8,8 +8,30 @@ const fieldMappings = [
     { source: 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(3) td:nth-child(5) a', destination: '#priorityComboboxInput' },
     { source: 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(3) td:nth-child(4)', destination: '#disclosureValueComboboxInput' },
     { source: 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(1) td:nth-child(12) span select option:checked', destination: '#sitePerformingLocComboboxInput' },
-    { source: 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(3) td:nth-child(6) input', destination: '#customerRequestDate' },
+    {
+        destination: '#customerRequestDate',
+        customFunction: () => {
+            const inputElement = document.querySelector('table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(3) td:nth-child(6) input');
+            if (inputElement) {
+                return { '#customerRequestDate': inputElement.value };
+            }
+            return {};
+        },
+    },
     { source: 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:first-child td:nth-child(16)', destination: '#customerRequestTime' },
+    { source: 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:first-child td:nth-child(16)', destination: '#orderTime' },
+    {
+        destination: '#ordDeskUser',
+        customFunction: (bemsId, siteRequesting) => {
+            return { '#ordDeskUser': bemsId };
+        },
+    },
+    {
+        destination: '#siteRequestingComboboxInput',
+        customFunction: (bemsId, siteRequesting) => {
+            return { '#siteRequestingComboboxInput': siteRequesting };
+        },
+    },
 ];
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -19,58 +41,87 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 });
 
+// Add the submit event listener code here
+const form = document.querySelector('div[ng-form="orderParamsForm"]'); // Replace 'div[ng-form="orderParamsForm"]' with the appropriate selector for the form on window 2
+if (form) {
+    console.log('Form element found:', form);
+    form.addEventListener('submit', (event) => {
+        console.log('Submit event triggered');
+        const priorityComboboxInput = document.querySelector('#priorityComboboxInput');
+        if (priorityComboboxInput) {
+            console.log('Value of #priorityComboboxInput before submission:', priorityComboboxInput.value);
+        }
+    });
+} else {
+    console.log('Form element not found');
+}
+
+
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
     console.log('Message received:', message);
 
     if (message.command === 'getSourceFields') {
+        const { bemsId, siteRequesting } = message;
         const sourceFields = {};
         fieldMappings.forEach((mapping) => {
-            const sourceElement = document.querySelector(mapping.source);
-            if (sourceElement) {
-                let value = sourceElement.textContent.trim();
+            // Handle the custom function for extension user bemsId and siteRequesting values
+            if (mapping.customFunction) {
+                const customValues = mapping.customFunction(bemsId, siteRequesting);
+                Object.assign(sourceFields, customValues);
+            } else {
+                const sourceElement = document.querySelector(mapping.source);
+                if (sourceElement) {
+                    let value = sourceElement.textContent.trim();
 
-                // handle #priorityComboboxInput to format text to captialize first letter
-                if (mapping.destination === '#priorityComboboxInput') {
-                    value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
-                }
-
-                // Handle the conditional mapping for the specific source field
-                if (mapping.source === 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(1) td:nth-child(12) span select option:checked') {
-                    if (value === 'S') {
-                        value = 'ST LOUIS';
-                    } else if (value === 'A') {
-                        value = 'SEATTLE';
-                    } else if (value === 'E') {
-                        value = 'EVERETT';
+                    // handle #priorityComboboxInput to format text to captialize first letter
+                    if (mapping.destination === '#priorityComboboxInput') {
+                        value = value.charAt(0).toUpperCase() + value.slice(1).toLowerCase();
                     }
-                }
 
-                // Handle the conditional mapping if #suppCode is empty
-                if (mapping.destination === '#suppCode' && !value) {
-                    mapping.source = 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(1) td:nth-child(14)'
-                }
+                    // Handle the conditional mapping for the specific source field
+                    if (mapping.source === 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(1) td:nth-child(12) span select option:checked') {
+                        if (value === 'S') {
+                            value = 'ST LOUIS';
+                        } else if (value === 'A') {
+                            value = 'SEATTLE';
+                        } else if (value === 'E') {
+                            value = 'EVERETT';
+                        }
+                    }
 
-                // Handle the conditional mapping if disclosureValueComboboxInput contains COM
-                if (mapping.destination === '#disclosureValueComboboxInput' && value.includes('COM')) {
-                    value = 'COM';
-                } else if (mapping.destination === '#disclosureValueComboboxInput' && value.includes('MIL')) {
-                    value = 'MIL';
-                }
+                    // Handle the conditional mapping if #suppCode is empty
+                    if (mapping.destination === '#suppCode' && !value) {
+                        mapping.source = 'table#bodyTable tr:not([style*="display: none"]) > td:first-child > table tr:nth-child(1) td:nth-child(14)'
+                    }
 
-                // Handle the date and time splitting
-                if (mapping.destination === '#customerRequestTime') {
-                    const [dateString, timeString] = value.split(' ');
-                    console.log('time value', value);
+                    // Handle the conditional mapping if disclosureValueComboboxInput contains COM or MIL
+                    if (mapping.destination === '#disclosureValueComboboxInput' && value.includes('COM')) {
+                        value = 'COM';
+                    } else if (mapping.destination === '#disclosureValueComboboxInput' && value.includes('MIL')) {
+                        value = 'MIL';
+                    }
+
+                    // Handle the date and time splitting for customerRequestDate
                     if (mapping.destination === '#customerRequestTime') {
-                        value = timeString;
+                        const [dateString, timeString] = value.split(' ');
+                        console.log('time value', value);
+                        if (mapping.destination === '#customerRequestTime') {
+                            value = timeString;
+                        }
                     }
-                }
-                // testing
-                if (mapping.destination === '#customerRequestDate') {
-                    console.log('date value', value);
-                }
+                    // Handle the date and time splitting for orderTime
+                    if (mapping.destination === '#orderTime') {
+                        const [dateString, timeString] = value.split(' ');
+                        console.log('time value', value);
+                        if (mapping.destination === '#orderTime') {
+                            value = timeString;
+                        }
+                    }
+                    
 
-                sourceFields[mapping.destination] = value;
+
+                    sourceFields[mapping.destination] = value;
+                }
             }
         });
         sendResponse({ sourceFields });
@@ -81,11 +132,11 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
             if (destinationElement && destinationFields[mapping.destination]) {
                 // Set the value of dropdown fields by selecting the corresponding option
                 if (destinationElement.tagName === 'SELECT') {
-                    const selectedIndex = Array.from(destinationElement.options).findIndex(option => option.value === destinationFields[mapping.destination]);
-                    if (selectedIndex !== -1) {
-                        destinationElement.selectedIndex = selectedIndex;
+                    const optionToSelect = Array.from(destinationElement.options).find(option => option.value === destinationFields[mapping.destination]);
+                    if (optionToSelect) {
+                        optionToSelect.selected = true;
 
-                        // Dispatch a change event for dropdown fields
+                        // Dispatch a change event to simulate a user selection
                         const changeEvent = new Event('change', { bubbles: true, cancelable: true });
                         destinationElement.dispatchEvent(changeEvent);
                     }
--- popup.js
+++ popup.js
@@ -1,4 +1,7 @@
 document.getElementById('copyFields').addEventListener('click', () => {
+    const bemsId = document.querySelector('#bemsId').value;
+    const siteRequesting = document.querySelector('#siteRequesting').value;
+
     browser.tabs.query({}).then((tabs) => {
         let sourceTab;
         let destinationTab;
@@ -12,7 +15,7 @@ document.getElementById('copyFields').addEventListener('click', () => {
         });
 
         if (sourceTab && destinationTab) {
-            browser.tabs.sendMessage(sourceTab.id, { command: 'getSourceFields' }).then((response) => {
+            browser.tabs.sendMessage(sourceTab.id, { command: 'getSourceFields', bemsId, siteRequesting }).then((response) => {
                 const { sourceFields } = response;
                 browser.tabs.sendMessage(destinationTab.id, { command: 'setDestinationFields', destinationFields: sourceFields });
             });
